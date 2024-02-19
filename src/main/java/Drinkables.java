a9aa9093907b10c9b48592a2a5d37e137c7f4d50
tnsapegina <143860753+tnsapegina@users.noreply.github.com>
2024-02-19 11:13:51 +0500

    Create Drinkables.java

---
+++ src/main/java/Drinkables.java
@@ -0,0 +1,20 @@
+
+public class Drinkables extends Product {
+    private Double volume;
+
+
+    public Drinkables(String name, BigDecimal price, Double volume) {
+        super(name, price);
+        this.volume = volume;
+    }
+
+    public Double getVolume() {
+        return volume;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "объем = " + volume + " ";
+    }
+
+}
