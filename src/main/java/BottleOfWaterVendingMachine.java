0e8ad1a90c7fdcdbfa8b99283073f6f08db5a85a
tnsapegina <143860753+tnsapegina@users.noreply.github.com>
2024-02-19 11:12:16 +0500

    Create BottleOfWaterVendingMachine.java

---
+++ src/main/java/BottleOfWaterVendingMachine.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BottleOfWaterVendingMachine implements VendingMachine{
+    List<Product> productList = new ArrayList<>();
+
+    public void addBottleOfWater(BottleOfWater bottle){
+        productList.add(bottle);
+    }
+    @Override
+    public void initProducts(List<Product> list) {
+        productList = list;
+    }
+
+    @Override
+    public Product getProduct(String name) {
+        for (Product i : productList) {
+            if(i.getName().equals(name))
+                return i;
+        }
+        return null;
+    }
+}
